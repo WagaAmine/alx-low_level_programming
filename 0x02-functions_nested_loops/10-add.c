939a682c06db81577f74a27150d3522c3a9e6232
WagaAmine <wagaamine417@gmail.com>
2023-07-18 09:15:24 -0700

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result DEMO
+ * @a: int to be added to b GLOBAL
+ * @b: int to be added to a DEMO
+ * Return: sum of a and b GG
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
